6fad457b3559e910b6efd0699e41ac3813ce17b3
DaviMattosDev <daviescolarj@gmail.com>
2024-04-30 20:56:51 -0300

    .

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import { ThemeProvider } from "@/components/theme-provider";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import ModeToggle from "@/components/color-toggle";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Zollak Chatbot",
+  description: "A chatbot that uses Gemini-Pro to respond to your messages.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} relative py-10`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="absolute top-5 right-5">
+            <ModeToggle />
+          </div>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
